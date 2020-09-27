318a8740beb5fd0b15315882bd1c565d31fdde31
Franky Liu <beyle931224@gmail.com>
2020-09-27 18:53:33 +0800

    Initial commit

---
+++ src/main/java/com/alex/Hello.java
@@ -0,0 +1,13 @@
+package com.alex;
+
+public class Hello {
+    public static void main(String[] args) {
+//        System.out.println("hello world..");
+
+//        new Person().hello();
+
+        Person p = new Person();
+        p.hello();
+
+    }
+}
---
+++ src/main/kotlin/com/alex/Hello.kt
@@ -0,0 +1,18 @@
+package com.alex
+
+fun main() {
+    println("Hello kotlin..")
+    Human().hello()
+
+    val h = Human()
+    h.hello()
+    
+
+
+}
+
+class Human(){
+    fun hello() {
+        println("Hello human")
+    }
+}
