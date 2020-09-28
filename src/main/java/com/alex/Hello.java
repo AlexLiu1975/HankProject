8b5957c9bc1748344584283de624d756d668b085
Franky Liu <beyle931224@gmail.com>
2020-09-28 09:22:54 +0800

    Java and Kotlin variable, data type

--- src/main/java/com/alex/Hello.java
+++ src/main/java/com/alex/Hello.java
@@ -9,5 +9,22 @@ public class Hello {
         Person p = new Person();
         p.hello();
 
+//      變數
+        int age = 10;
+        char c = '我';
+        byte  b = 120;
+        float weight = 66.5f;
+        boolean adult = true;
+        boolean enroll = false;
+        String name = "alex";
+
+//      類別
+        Integer age2 = 19;
+        System.out.println(age2.equals("Alex"));
+
+
+        System.out.println();
+
+
     }
 }
--- src/main/kotlin/com/alex/Hello.kt
+++ src/main/kotlin/com/alex/Hello.kt
@@ -6,7 +6,23 @@ fun main() {
 
     val h = Human()
     h.hello()
-    
+
+//    變數
+/*
+    val 不可變的
+    var 可變的
+ */
+
+    var age = 19;
+//  自動推斷
+    var age2: Int = 20;
+    var weight : Float = 66.5f;
+    var name : String;
+    name = "Alex"
+    println(name)
+
+
+
 
 
 }
